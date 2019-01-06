925c58d884d26ff2f7e0705e926096f0faccaf23
May Guo <40670609+windcloak@users.noreply.github.com>
2019-01-06 16:29:07 -0600

    Create Monster_Gator.java

---
+++ package02/Monster_Gator.java
@@ -0,0 +1,11 @@
+package package02;
+
+public class Monster_Gator extends SuperMonster {
+	public Monster_Gator() {
+		super.setName("Gator");
+		super.setHP(20);
+		super.setAtk(6);
+		super.setAtkMsg("The gator bit you!");
+
+	}
+}
